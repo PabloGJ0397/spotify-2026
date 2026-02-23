46d13ec41375f0027b65745cc375a776071e332e
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 17:53:28 +0100

    feat(album): add AlbumMemLocalDataSource with singleton pattern

---
+++ src/main/java/dev/pablogj/spotify2026/albums/data/AlbumDataRepository.java
@@ -0,0 +1,11 @@
+package dev.pablogj.spotify2026.albums.data;
+
+import dev.pablogj.spotify2026.albums.domain.AlbumRepository;
+
+public class AlbumDataRepository implements AlbumRepository {
+
+    private AlbumRepository albumRepository;
+    public AlbumDataRepository(AlbumRepository albumRepository) {
+        t
+    }
+}
