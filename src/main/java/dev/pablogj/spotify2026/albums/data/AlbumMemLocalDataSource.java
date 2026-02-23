d1b399ebb9b907c25203a7c7f77f184c0daf169a
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 19:01:55 +0100

    fix: AlbumMemLocalDataSource

--- src/main/java/dev/pablogj/spotify2026/albums/data/AlbumMemLocalDataSource.java
+++ src/main/java/dev/pablogj/spotify2026/albums/data/AlbumMemLocalDataSource.java
@@ -7,20 +7,20 @@ import java.util.ArrayList;
 public class AlbumMemLocalDataSource {
     private static AlbumMemLocalDataSource instance = null;
 
-    private ArrayList<Album> storage = new ArrayList<>();
+    private ArrayList<Album> albumMemStorage = new ArrayList<>();
 
     private AlbumMemLocalDataSource() {
         initData();
     }
     private void initData() {
-        storage.add(new Album ("1","Abbey Road", "1969"));
-        storage.add(new Album ("2","Escape Plan", "2021"));
+        albumMemStorage.add(new Album ("1","Abbey Road", "1969"));
+        albumMemStorage.add(new Album ("2","Escape Plan", "2021"));
     }
     public ArrayList<Album> findAll() {
-        return storage;
+        return albumMemStorage;
     }
     public void save(Album album) {
-        storage.add(album);
+        albumMemStorage.add(album);
     }
     public static AlbumMemLocalDataSource getInstance() {
         if (instance == null) {
