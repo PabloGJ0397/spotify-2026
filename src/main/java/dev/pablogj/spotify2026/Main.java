32b9754dd274242cae1ea5cec190e4e14ebe0621
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 21:40:44 +0100

    feat(authors): add Singleton pattern to AuthorMemLocalDataSource

--- src/main/java/dev/pablogj/spotify2026/Main.java
+++ src/main/java/dev/pablogj/spotify2026/Main.java
@@ -1,23 +1,31 @@
 package dev.pablogj.spotify2026;
 
+import dev.pablogj.spotify2026.albums.presentation.AlbumView;
 import dev.pablogj.spotify2026.authors.presentation.AuthorView;
 import dev.pablogj.spotify2026.songs.presentation.SongView;
 
 public class Main {
+
+    /*
     // TEST SONGS FEATURE FROM MAIN
+
+
     public static void main(String[] args) {
         SongView.printSongs();
     }
+    */
 
-    /*TEST ALBUMS FEATURE FROM MAIN
+    /*
+    //TEST ALBUMS FEATURE FROM MAIN
+
     public static void main(String[] args) {
         AlbumView.saveAlbum();
     }
+    */
 
-     */
-    /*
+    //TEST AUTHORS FROM MAIN
     public static void main(String[] args) {
         AuthorView.printAuthors();
     }
-*/
+
 }
