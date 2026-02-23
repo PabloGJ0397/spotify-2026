f93d3a29836a5cd73508c3dd9ff0f53db7e025b9
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 20:57:19 +0100

    chore(songs): test Singleton instance from Main

--- src/main/java/dev/pablogj/spotify2026/Main.java
+++ src/main/java/dev/pablogj/spotify2026/Main.java
@@ -1,22 +1,23 @@
 package dev.pablogj.spotify2026;
 
 import dev.pablogj.spotify2026.authors.presentation.AuthorView;
+import dev.pablogj.spotify2026.songs.presentation.SongView;
 
 public class Main {
-    /* TEST SONGS FEATURE FROM MAIN
+    // TEST SONGS FEATURE FROM MAIN
     public static void main(String[] args) {
         SongView.printSongs();
     }
 
-     */
     /*TEST ALBUMS FEATURE FROM MAIN
     public static void main(String[] args) {
         AlbumView.saveAlbum();
     }
 
      */
+    /*
     public static void main(String[] args) {
         AuthorView.printAuthors();
     }
-
+*/
 }
