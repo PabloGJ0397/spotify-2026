ff16d8f8edf4ea7a02c321232e7f6ab2dd7672a6
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 18:07:33 +0100

    chore(album): test albums feature from main

--- src/main/java/dev/pablogj/spotify2026/Main.java
+++ src/main/java/dev/pablogj/spotify2026/Main.java
@@ -1,10 +1,18 @@
 package dev.pablogj.spotify2026;
 
+import dev.pablogj.spotify2026.albums.presentation.AlbumView;
 import dev.pablogj.spotify2026.songs.presentation.SongView;
 
 public class Main {
+    /* TEST SONGS FEATURE FROM MAIN
     public static void main(String[] args) {
         SongView.printSongs();
+    }
+
+     */
+
+    public static void main(String[] args) {
+        AlbumView.saveAlbum();
     }
 
 }
