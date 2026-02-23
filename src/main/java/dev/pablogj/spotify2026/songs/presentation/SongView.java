65ca5c671d175af9a2a4ab17024e72385c223a1c
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 22:01:36 +0100

    feat(songs): add saveSong method to SongView

--- src/main/java/dev/pablogj/spotify2026/songs/presentation/SongView.java
+++ src/main/java/dev/pablogj/spotify2026/songs/presentation/SongView.java
@@ -3,6 +3,7 @@ package dev.pablogj.spotify2026.songs.presentation;
 import dev.pablogj.spotify2026.songs.data.SongDataRepository;
 import dev.pablogj.spotify2026.songs.data.SongsMemLocalDataSource;
 import dev.pablogj.spotify2026.songs.domain.GetSongsUseCase;
+import dev.pablogj.spotify2026.songs.domain.SaveSongUseCase;
 import dev.pablogj.spotify2026.songs.domain.Song;
 import dev.pablogj.spotify2026.songs.domain.SongRepository;
 
@@ -13,5 +14,11 @@ public class SongView {
         GetSongsUseCase getSongsUseCase = new GetSongsUseCase(new SongDataRepository(SongsMemLocalDataSource.getInstance()));
          ArrayList<Song> songs = getSongsUseCase.execute();
         System.out.println("Songs: \n" + songs);
+    }
+    public static void saveSong(){
+        Song songTest = new Song("3", "Hotel California", "Eagles", "6:30");
+        SaveSongUseCase saveSongUseCase = new SaveSongUseCase(new SongDataRepository(SongsMemLocalDataSource.getInstance()));
+        saveSongUseCase.execute(songTest);
+        printSongs();
     }
 }
