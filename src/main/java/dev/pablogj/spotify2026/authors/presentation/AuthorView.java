5df04965bb58428aaff0b8dfded3c76c80eb6b8c
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 19:23:50 +0100

    fix: Author View

--- src/main/java/dev/pablogj/spotify2026/authors/presentation/AuthorView.java
+++ src/main/java/dev/pablogj/spotify2026/authors/presentation/AuthorView.java
@@ -1,15 +1,20 @@
 package dev.pablogj.spotify2026.authors.presentation;
 
+import dev.pablogj.spotify2026.albums.domain.Album;
 import dev.pablogj.spotify2026.authors.data.AuthorApiLocalDataSource;
 import dev.pablogj.spotify2026.authors.data.AuthorDataRepository;
 import dev.pablogj.spotify2026.authors.data.AuthorMemLocalDataSource;
 import dev.pablogj.spotify2026.authors.domain.Author;
 import dev.pablogj.spotify2026.authors.domain.GetAuthorsUseCase;
 
-public class AuthorsView {
+import java.util.ArrayList;
+
+public class AuthorView {
     public static void printAuthors(){
         GetAuthorsUseCase getAuthorsUseCase = new GetAuthorsUseCase
                 (new AuthorDataRepository(AuthorMemLocalDataSource.getInstance(), AuthorApiLocalDataSource.getInstance()));
 
+        ArrayList<Author> authors = getAuthorsUseCase.execute();
+        System.out.println(authors);
     }
 }
