35c12ed05deeecf7d9f75373091d16731054f5bf
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 22:01:14 +0100

    feat(songs): add SaveSongUseCase

---
+++ src/main/java/dev/pablogj/spotify2026/songs/domain/SaveSongUseCase.java
@@ -0,0 +1,13 @@
+package dev.pablogj.spotify2026.songs.domain;
+
+public class SaveSongUseCase {
+    private SongRepository songRepository;
+
+    public SaveSongUseCase(SongRepository songRepository) {
+        this.songRepository = songRepository;
+    }
+    public void execute(Song song){
+        songRepository.saveSong(song);
+    }
+
+}
