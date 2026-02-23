c6dbf775f47a24291607398d3dc34dedd44fcf6a
PabloGJ0397 <garciajuarezpablo97@gmail.com>
2026-02-23 20:55:40 +0100

    refactor(songs): update SongView to use Singleton instance

--- src/main/java/dev/pablogj/spotify2026/songs/presentation/SongView.java
+++ src/main/java/dev/pablogj/spotify2026/songs/presentation/SongView.java
@@ -1,6 +1,7 @@
 package dev.pablogj.spotify2026.songs.presentation;
 
 import dev.pablogj.spotify2026.songs.data.SongDataRepository;
+import dev.pablogj.spotify2026.songs.data.SongsMemLocalDataSource;
 import dev.pablogj.spotify2026.songs.domain.GetSongsUseCase;
 import dev.pablogj.spotify2026.songs.domain.Song;
 import dev.pablogj.spotify2026.songs.domain.SongRepository;
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 
 public class SongView {
     public static void printSongs(){
-        GetSongsUseCase getSongsUseCase = new GetSongsUseCase(new SongDataRepository());
+        GetSongsUseCase getSongsUseCase = new GetSongsUseCase(new SongDataRepository(SongsMemLocalDataSource.getInstance()));
          ArrayList<Song> songs = getSongsUseCase.execute();
         System.out.println("Songs: \n" + songs);
     }
